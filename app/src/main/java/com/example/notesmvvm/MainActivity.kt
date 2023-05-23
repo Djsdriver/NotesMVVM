952d4b05ffdff8aa6c13b93a2c5c93181d35f620
Djsdriver <alexei.gra4ev@gmail.com>
2023-05-23 17:11:08 +0300

    урок 5

--- app/src/main/java/com/example/notesmvvm/MainActivity.kt
+++ app/src/main/java/com/example/notesmvvm/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.notesmvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.notesmvvm.databinding.ActivityMainBinding
+import com.example.notesmvvm.utilits.APP_ACTIVITY
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mToolbar: Toolbar
+    private lateinit var mNavController: NavController
+    private var _binding: ActivityMainBinding?=null
+
+    val mBinding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        APP_ACTIVITY=this
+
+        mToolbar=mBinding.toolbar
+        mNavController=Navigation.findNavController(this, R.id.toolbar)
+        setSupportActionBar(mToolbar)
+        title="Заметки"
+
+
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding=null
     }
 }
--- app/src/main/java/com/example/notesmvvm/screens/AddNewNoteFragment.kt
+++ app/src/main/java/com/example/notesmvvm/screens/AddNewNoteFragment.kt
@@ -16,7 +16,7 @@ class AddNewNoteFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        return inflater.inflate(R.layout.fragment_add_note, container, false)
     }
 
 
